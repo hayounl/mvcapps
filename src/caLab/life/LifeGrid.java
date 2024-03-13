570746673635f44c2bd5ea7d673f6b8aac92a312
Hayounl061 <hayounl061@gmail.com>
2024-03-13 11:37:01 -0700

    grid factory updated

--- src/caLab/life/LifeGrid.java
+++ src/caLab/life/LifeGrid.java
@@ -4,6 +4,9 @@ import CALab.Cell;
 import CALab.Grid;
 
 public class LifeGrid extends Grid {
+    public LifeGrid(int dim){
+        super();
+    }
     @Override
     public Cell makeCell(boolean uniform) {
         return null;
