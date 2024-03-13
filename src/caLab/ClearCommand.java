d10fa817e1eb54e6a4c5441560fc586f444ad433
Hayounl061 <hayounl061@gmail.com>
2024-03-13 11:23:11 -0700

    update gridpanel to work with app panel

--- src/caLab/ClearCommand.java
+++ src/caLab/ClearCommand.java
@@ -1,4 +1,8 @@
 package CALab;
+import mvc.*;
+public class ClearCommand extends Command{
+    public ClearCommand(Model model){super(model);}
+    public void execute(){
 
-public class ClearCommand {
+    }
 }
