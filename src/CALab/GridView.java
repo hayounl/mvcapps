0c2f3a792fc7b1ebf4f2e5855fb2cf326da7fab2
Alvin Dang <alvin.dang@yahoo.com>
2024-03-17 22:10:28 -0700

    Refactor CellView & GridView
    Rewrote how CellView and GridView work so that they match the interpretation set by Professor's example
    
    DM me if you need an explanation

--- src/CALab/GridView.java
+++ src/CALab/GridView.java
@@ -6,16 +6,25 @@ import java.awt.*;
 
 public class GridView extends View {
     private Grid grid; // Added by Hayoun
-    private CellView cellViews[][];
+    private CellView cellViews[][]; // 2D Array to represent Grid of CellView JButtons
 
+    /* The GridView component is literally a Grid of CellView objects.
+           A CellView is a JButton
+           This means GridView should render a Grid of JButtons (CellView's)
+                so that each CellView can listen for actions performed on it.
+     */
     public GridView(Grid grid) {
         super(grid);
         this.grid = grid;
-        int len = grid.getDim();
-        cellViews = new CellView[len][len];
-        setLayout(new GridLayout(len, len));
+        int dim = grid.getDim();
+        cellViews = new CellView[dim][dim];
+        setLayout(new GridLayout(dim, dim));
 
+        // Version 2: commented out line 21 code bit
+        initializeCellViews(dim); // method to handle creating cellViews[][]
+        // update(); // Call update after CellViews have been initialized
         // Fill in 'cellViews' array and then add 'cellViews' to the GridView
+        /*
         for(int i = 0; i < len; i++) {
             for(int j = 0; j < len; j++) {
                 Cell cell = grid.getCell(i, j);
@@ -38,6 +47,7 @@ public class GridView extends View {
                 add(cellView);
             }
         }
+        */
 
         /*
         Cell cell = new CellView(((Grid)model).getCell(row, col)
@@ -65,7 +75,27 @@ public class GridView extends View {
             }
 
         }*/
+    }
 
+    private void initializeCellViews(int gridDim) {
+        for (int i = 0; i < gridDim; i++) {
+            for (int j = 0; j < gridDim; j++) {
+                Cell cell = grid.getCell(i, j); // get Cell obj at this pos in the grid
+                CellView cellView = new CellView(cell); // Associate the Cell object with CellView instance
+                cellViews[i][j] = cellView; // Assign CellView instance to the CellView 2D array
+                add(cellView); // Add CellView instance to GridView for display
+            }
+        }
+    }
+
+    // Added in version 2 to follow professor's partial implementation of GridView
+    public void update() {
+        for (int i = 0; i < grid.getDim(); i++) {
+            for (int j = 0; j < grid.getDim(); j++) {
+                CellView cellView = cellViews[i][j];
+                cellView.update(); // Call the update method of each CellView
+            }
+        }
     }
 
     /*class TileView extends JLabel {
@@ -77,8 +107,8 @@ public class GridView extends View {
 
         }
     }*/
-    public void update(String msg, Object oldState, Object newState) {
+    /*public void update(String msg, Object oldState, Object newState) {
         // call update method of each CellView
-    }
+    }*/
 
 }
