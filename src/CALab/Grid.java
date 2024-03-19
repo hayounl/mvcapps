62e7f269c4d4c5661ab3de0cf8c50a4e825433c6
Alvin Dang <alvin.dang@yahoo.com>
2024-03-18 20:05:31 -0700

    Implement Clear function
    Added clear action listener and implemented clear method in grid.java
    
    Also moved populate() to grid.java and re-implemented repopulate() in grid.java
    
    Fixed getNeighbors() and cleaned up code in grid.java

--- src/CALab/Grid.java
+++ src/CALab/Grid.java
@@ -10,18 +10,67 @@ public abstract class Grid extends Model {
     static private int time = 0;
     protected int dim = 20;
     protected Cell[][] cells;
+
     public int getDim() { return dim; }
     public int getTime() { return time; }
     public Cell getCell(int row, int col) { return cells[row][col]; }
     public abstract Cell makeCell(int row, int col);
+
     public Grid(int dim) {
         this.dim = dim;
         cells = new Cell[dim][dim];
         populate();
     }
     public Grid() { this(20); }
-
-    public abstract void populate();
+    public void populate() {
+        //Loop through entire grid cell by cell & populate it
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                // 1. use makeCell to fill in cells
+                cells[i][j] = makeCell(i, j);
+            }
+        }
+        // Call repopulate to set the status of each cell
+        repopulate(true);
+        // Loop through entire grid cell by cell & set ambience levels by calling observe()
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j].observe();
+            }
+        }
+    }
+    public void clear() {
+        repopulate(false);
+        System.out.println("Called Repopulate(false)!");
+    }
+    // Can be called by Populate OR Clear
+    //      randomly = True means we are repopulating randomly
+    //      randomly = False means we are 'clearing' the grid
+    public void repopulate(boolean randomly) {
+        if (randomly) {
+            // Loop through entire grid cell by cell & randomly set the status of each cell
+            Random random = new Random();
+            for (int i = 0; i < cells.length; i++) {
+                for (int j = 0; j < cells[i].length; j++) {
+                    Cell newCell = cells[i][j];
+                    Agent agent = (Agent) newCell;
+                    agent.setStatus(random.nextInt(2));
+                }
+            }
+        } else {
+            // Loop through entire grid cell by cell & set the status of each cell to 0 (dead)
+            for (int i = 0; i < cells.length; i++) {
+                for (int j = 0; j < cells[i].length; j++) {
+                    Cell newCell = cells[i][j];
+                    Agent agent = (Agent) newCell;
+                    agent.setStatus(0);
+                }
+            }
+        }
+        observe();
+        // notify subscribers
+        notifySubscribers();
+    }
 
     public Set<Cell> getNeighbors(Cell asker, int radius) {
         /*
@@ -35,18 +84,30 @@ public abstract class Grid extends Model {
         int col = asker.col;
         int n = dim -1;
         int top, below, left, right;
+
         if(row == 0) {
             top = n;
         } else {
             top = (row-1)%n;
         }
-        below = (row+1)%n;
+
+        if(row == n) {
+            below = 0;
+        } else {
+            below = (row+1)%n;
+        }//below = (row+1)%n;
+
         if(col == 0) {
             left = n;
         } else {
             left = (col-1)%n;
         }
-        right = (col+1)%n;
+
+        if(col == n) {
+            right = 0;
+        } else {
+            right = (col+1)%n;
+        }//right = (col+1)%n;
         //row above
         myNeighbors.add(cells[top][left]);
         myNeighbors.add(cells[top][col]);
@@ -62,9 +123,6 @@ public abstract class Grid extends Model {
         return myNeighbors;
     }
 
-    // overide these
-    public int getStatus() { return 0; }
-    public Color getColor() { return Color.GREEN; }
     // cell phases:
     public void observe() {
         // call each cell's observe method and notify subscribers
@@ -91,6 +149,7 @@ public abstract class Grid extends Model {
         }
         notifySubscribers();
     }
+
     public void updateLoop(int cycles) {
         observe();
         for(int cycle = 0; cycle < cycles; cycle++) {
@@ -98,10 +157,11 @@ public abstract class Grid extends Model {
             update();
             observe();
             time++;
-            //System.out.println("time = " + time);
+            System.out.println("time = " + time);
             Agent newAgent = (Agent)cells[1][1];
-            System.out.println(newAgent.getAmbience());
             notifySubscribers();
+
+            System.out.println(newAgent.getAmbience());
         }
     }
 }
